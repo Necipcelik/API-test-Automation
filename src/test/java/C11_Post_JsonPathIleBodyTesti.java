90988aecd8c17ce5e8262a4ba1d2b5393a413d78
Necip <necipcelik16@web.de>
2023-03-17 17:28:15 +0100

    JsonPath kullanimi dahil

---
+++ src/test/java/C11_Post_JsonPathIleBodyTesti.java
@@ -0,0 +1,71 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class C11_Post_JsonPathIleBodyTesti {
+    @Test
+    public void test10() {
+        /*
+        https://restful-booker.herokuapp.com/booking url’ine asagidaki body'ye sahip bir POST request
+gonderdigimizde
+{
+"firstname" : "Ahmet",
+"lastname" : “Bulut",
+"totalprice" : 500,
+"depositpaid" : false,
+"bookingdates" : {
+"checkin" : "2023-01-10",
+"checkout" : "2023-01-20"
+},
+"additionalneeds" : "wi-fi"
+}
+POST REQUEST, RESPONSE BODY BILGILERINI
+ASSERT YAPARKEN JSONPATH KULLANMA
+donen Response’un,
+status code’unun 200,
+ve content type’inin application-json,
+ve response body’sindeki
+"firstname“in,"Ahmet",
+ve "lastname“in, "Bulut",
+ve "totalprice“in,500,
+ve "depositpaid“in,false,
+ve "checkin" tarihinin 2023-01-10
+ve "checkout" tarihinin 2023-01-20
+ve "additionalneeds“in,"wi-fi"
+oldugunu test edin
+         */
+        String url = " https://restful-booker.herokuapp.com/booking";
+        JSONObject reqestBody = new JSONObject();
+        JSONObject rezervasyonTarihleri = new JSONObject();
+        rezervasyonTarihleri.put("checkin", "2023-01-10");
+        rezervasyonTarihleri.put("checkout", "2023-01-20");
+
+        reqestBody.put("firstname", "Ahmet");
+        reqestBody.put("lastname", "Bulut");
+        reqestBody.put("totalprice", 500);
+        reqestBody.put("depositpaid", false);
+        reqestBody.put("bookingdates", rezervasyonTarihleri);
+        reqestBody.put("additionalneeds", "wi-fi");
+
+        Response response = given().contentType(ContentType.JSON)
+                           .when().body(reqestBody.toString())
+                           .post(url);
+        response.prettyPrint();
+
+        response.then().assertThat()
+                .statusCode(200)
+                .contentType(ContentType.JSON).
+                body("booking.firstname", equalTo("Ahmet"),"booking.lastname",equalTo("Bulut")
+                        ,"booking.totalprice",equalTo(500),
+                        "booking.depositpaid",equalTo(false),
+                        "booking.bookingdates.checkin",equalTo("2023-01-10"),
+                        "booking.bookingdates.checkout",equalTo("2023-01-20"));
+
+
+    }
+}
---
+++ src/test/java/C13_Get_ExpectedDataOlusturma.java
@@ -0,0 +1,44 @@
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class C13_Get_ExpectedDataOlusturma {
+    @Test
+    public void test12(){
+        /*
+        https://jsonplaceholder.typicode.com/posts/22 url'ine bir GET request
+        yolladigimizda donen response body’sinin asagida verilen ile ayni oldugunutest ediniz
+           Response body :
+        {
+          "userId": 3,
+            "id": 22,
+          "title": "dolor sint quo a velit explicabo quia nam",
+          "body": "eos qui et ipsum ipsam suscipit autsed omnis non odioexpedita ear
+              um mollitia molestiae aut atque rem suscipitnam impedit esse"
+                  }
+         */
+
+          String url="https://jsonplaceholder.typicode.com/posts/22";
+
+        JSONObject expectedData=new JSONObject();
+        expectedData.put("userId", 3);
+        expectedData.put("id", 22);
+        expectedData.put("title", "dolor sint quo a velit explicabo quia nam");
+        expectedData.put("body","eos qui et ipsum ipsam suscipit aut\nsed omnis non odio\nexpedita earum mollitia molestiae aut atque rem suscipit\nnam impedit esse"
+
+               );
+
+        Response response=given().when().get(url);
+        response.prettyPrint();
+        JsonPath responseJsonPath=response.jsonPath();
+
+        Assert.assertEquals(expectedData.get("id"),responseJsonPath.get("id"));
+        Assert.assertEquals(expectedData.get("userId"),responseJsonPath.get("userId"));
+        Assert.assertEquals(expectedData.get("title"),responseJsonPath.get("title"));
+        Assert.assertEquals(expectedData.get("body"),responseJsonPath.get("body"));
+    }
+}
